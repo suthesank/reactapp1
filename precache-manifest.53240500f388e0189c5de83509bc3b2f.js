a2783062ac05c9de8ecc73fd552e42e3e20fe982
suthesan <suthesan@DESKTOP-S4H1U0J>
2020-02-20 16:46:53 +0800

    First commit

---
+++ precache-manifest.53240500f388e0189c5de83509bc3b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75ed111eaca6cde5e7931bf46f2a033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8904800790dca3bd345",
+    "url": "/static/js/2.c50c2c28.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c50c2c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924574b29c55490b89ec",
+    "url": "/static/js/main.0e1f6dfb.chunk.js"
+  },
+  {
+    "revision": "34ac840d18bfff9e1815",
+    "url": "/static/js/runtime-main.787bf480.js"
+  }
+]);
